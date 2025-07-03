61443e8eb47633f1d92fef532c65d3965f837107
nokokiii <kubanokoki@gmail.com>
2025-07-03 13:57:27 +0200

    styling: Renaming folder name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="fastapi_utils",
+    name="fastapi-utils",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
