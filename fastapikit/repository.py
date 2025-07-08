611b35fbf983c925aa77507e15ee075e1379ab97
nokokiii <kubanokoki@gmail.com>
2025-07-08 23:29:22 +0200

    feat: Transactions method added to Repo

--- fastapikit/repository.py
+++ fastapikit/repository.py
@@ -2,7 +2,7 @@ from typing import Callable, Type, TypeVar
 
 from fastapi import Depends
 
-from .database import Driver
+from .database import Driver, handle_db_results
 
 
 class Repository:
@@ -13,6 +13,27 @@ class Repository:
         if not conn or not hasattr(conn, "db"):
             raise ValueError("Invalid Driver instance passed to repository.")
         self.db = conn.db
+
+    @handle_db_results(serialize=False)
+    async def begin_transaction(self) -> None:
+        """
+        Function to begin transaction
+        """
+        await self.db.query("BEGIN TRANSACTION")
+    
+    @handle_db_results(serialize=False)
+    async def commit_transcation(self) -> None:
+        """
+        Function to commit transaction
+        """
+        await self.db.query("COMMIT TRANSACTION")
+    
+    @handle_db_results(serialize=False)
+    async def cancel_transaction(self) -> None:
+        """
+        Function to cancel transaction
+        """
+        await self.db.query("CANCEL TRANSACTION")
 
 
 RepoType = TypeVar('RepoType', bound='Repository')
